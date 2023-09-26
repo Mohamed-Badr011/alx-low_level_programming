69eba612f7f6fbc7c3959413b6ba8a18e70cc4a6
Mohamed Badr <mouhamedebrahim1@gmail.com>
2023-09-26 13:38:25 -0700

    0x00-hello_world is done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - print a string using puts() function
+ *
+ * Return - 0 (error)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
